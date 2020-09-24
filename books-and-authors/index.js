5945ead84bc42c3f1d8a836195b66996db97f80d
Jacobtco <jacobtco@gmail.com>
2020-09-24 17:39:36 +0800

    Added Book and Query(bookCount and authorCount) typeDef

--- books-and-authors/index.js
+++ books-and-authors/index.js
@@ -85,11 +85,23 @@ let books = [
 
 const typeDefs = gql`
   type Query {
+    bookCount: Int!
+    authorCount: Int!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
   }
 `
 
 const resolvers = {
   Query: {
+    bookCount: () => books.length,
+    authorCount: () => books.length
   }
 }
 
